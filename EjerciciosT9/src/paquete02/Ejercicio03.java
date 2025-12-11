16c3ec08280f9f64825c1baea0f19aa519845ba1
jdguerrero28 <jdiegogueal@gmail.com>
2025-12-11 15:32:35 -0500

    Ejercicio03

--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -17,7 +17,30 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        
+        double suma = 0;
+        double media = 0;
+        int mediaEncima = 0;
+        int mediaAbajo = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+
+        }
+        media = suma / arreglo.length;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] > media) {
+                mediaEncima = mediaEncima + 1;
+            } else {
+                mediaAbajo = mediaAbajo + 1;
+            }
+        }
+        System.out.printf("La media es:%.2f\n"
+                + "Elementos por encima de la media:%d\n"
+                + "Elementos por debajo de la media:%d\n",
+                media,
+                mediaEncima,
+                mediaAbajo);
     }
 
 }
